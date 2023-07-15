18404e3547d80b1ee8bd84359eb590c41dc2faab
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-07-16 01:08:12 +0800

    feat:main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import ButtonHome from "./components/ButtonHome";
+import Loader from "./components/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && <Loader />}
+      <main className="text-gray-400 bg-gray-900 flex flex-col gap-40 pb-20">
+        <Hero />
+        <Projects />
+        <Skills />
+        <ButtonHome />
+      </main>
+    </>
   );
 }
 
